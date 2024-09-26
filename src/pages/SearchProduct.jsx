c27f43262ab1e77f870ab27d74468234aa4930f1
Kyaw San Oo <skyaw6736@gmail.com>
2024-09-26 16:11:27 +0630

    fixed some error

--- src/pages/SearchProduct.jsx
+++ src/pages/SearchProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useLocation, useParams } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import SummerApi from '../common'
 import SearchVerticalProductCard from '../components/SearchVerticalProductCard'
 
